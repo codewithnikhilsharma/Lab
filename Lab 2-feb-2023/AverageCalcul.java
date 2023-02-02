717bc7c41e5a9eaa7bf2664dca99d8e536da8c8b
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-02 19:10:04 +0530

    Add files via upload

---
+++ Lab 2-feb-2023/AverageCalcul.java
@@ -0,0 +1,29 @@
+package Object;
+
+public class AverageCalcul {
+	
+	// Java program that takes five numbers as input to calculate and print
+	// the average of the numbers.
+   
+	public static void main(String[] args) {
+		
+		
+		// Input
+		
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int num4 = 40;
+		int num5 = 50;
+		
+		int Average;
+		
+		 
+		// Calculate Average
+		 Average = (num1 + num2 + num3 + num4 + num5) / 5;
+		 
+		 // For output
+		 System.out.println("The average of the five numbers is: " + Average);
+	
+	}
+}
