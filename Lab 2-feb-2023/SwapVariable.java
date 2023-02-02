7ea949f02ecc34fbb6ed4ae7e5e2d33dacc8a2de
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-02 19:14:38 +0530

    Add files via upload

---
+++ Lab 2-feb-2023/SwapVariable.java
@@ -0,0 +1,22 @@
+package Object;
+
+public class SwapVariable {
+    
+	// Swapping two variables
+	
+	public static void main(String[] args) {
+		
+		   // input value
+			    int x = 10;
+			    int y = 20;
+
+			    System.out.println("Before swapping: x = " + x + " and y = " + y);
+
+			    int temp = x;
+			    x = y;
+			    y = temp;
+
+			    System.out.println("After swapping: x = " + x + " and y = " + y);
+			  }		
+	        }
+
