e525b57f02f32f067cd2ed9713a36278a2be6587
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-02 19:13:56 +0530

    Add files via upload

---
+++ Lab 2-feb-2023/Expression.java
@@ -0,0 +1,26 @@
+package Object;
+
+public class Expression {
+   
+      // Write a Java program to compute the specified expressions and print the output.
+	
+		  public static void main(String[] args) {
+			  
+			  // input value taking double data type because its a decimal value
+			  
+		    double num1 = 25.5;
+		    double num2 = 3.5;
+		    double num3 = 40.5;
+		    double num4 = 4.5;
+            
+		    // Expression
+		    double result = (num1 * num2 - num2 * num2) / (num3 - num4);
+		    
+		    // output
+		    System.out.println("Result: " + result);
+		  }
+    	}
+		  
+		
+
+
