aa35af87e375285a2cf19264763caa9676ec8856
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-09 17:12:43 +0530

    Add files via upload

---
+++ Lab.09.03.2023/PrintingOddElement.java
@@ -0,0 +1,16 @@
+package NLab;
+
+public class PrintingOddElement {
+
+  public static void main(String[] args) {
+	  // Define an array of Strings
+	   String[] arr = {"a", "b", "c", "d", "e"};
+
+   for (int i = 0; i < arr.length; i += 2) {
+	   // Print the current element at index i
+     System.out.println(arr[i]);
+	  }
+	    }
+	  }
+
+
